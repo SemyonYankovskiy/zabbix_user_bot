d18774d496d9e05ebd59c57c9c62fcb31821732a
Игорь Руденко <ig.rudenko1@yandex.ru>
2025-09-05 16:14:19 +0300

    update

--- app/service/ecstasy.py
+++ app/service/ecstasy.py
@@ -1,93 +1,32 @@
 import asyncio
 import os
-from typing import Optional, Dict, List
+from typing import Dict, List
 
 import aiohttp
 
 
-class EcstasyConnector:
-    _url = os.getenv("ECSTASY_URL")
-    _login = os.getenv("ECSTASY_LOGIN")
-    _password = os.getenv("ECSTASY_PASSWORD")
-
-    def __init__(self):
-        self._raw_session: Optional[aiohttp.ClientSession] = None
-        self._tokens: Dict[str, str] = {}
-
-    async def request(self, method: str, url: str, **kwargs):
-        """
-        Функция выполняет HTTP-запрос с обработкой аутентификации.
-        """
-        if not self._tokens:
-            await self._do_auth()
-
-        kwargs["headers"] = self._add_token(kwargs.get("headers"))
-
-        resp = await self._session.request(method, self._url + url, **kwargs)
-        if resp.status in [401, 403]:
-            await self._do_auth()
-            kwargs["headers"] = self._add_token(kwargs.get("headers"))
-            resp = await self._session.request(method, self._url + url, **kwargs)
-        return resp
-
-    def _add_token(self, headers: Optional[Dict[str, str]]) -> Dict[str, str]:
-        if headers is None:
-            headers = {}
-        headers.update({"Authorization": f"Bearer {self._tokens.get('access')}"})
-        return headers
-
-    async def _do_auth(self):
-        # Для начало проверяем, имеется ли возможность обновить access token через refresh token
-        if self._tokens.get("refresh"):
-            resp = await self._session.post(
-                self._url + "/api/token/refresh",
-                json={"refresh": self._tokens.get("refresh")},
-            )
-            print("DO AUTH", resp.status)
-            if resp.status == 200:
-                self._tokens = await resp.json()
-                resp.close()
-                return
-            else:
-                resp.close()
-
-        # Если не удалось обновить через refresh token
-        resp = await self._session.post(
-            self._url + "/api/token",
-            json={"username": self._login, "password": self._password},
-        )
-        if resp.status == 200:
-            self._tokens = await resp.json()
-        resp.close()
+class EcstasyService:
+    def __init__(self, base_url: str = "", token: str = ""):
+        self._base_url = base_url
+        self._token = token
+        self.headers = {"Authorization": f"Token {self._token}"}
+        self._session = None
 
     @property
-    def _session(self) -> aiohttp.ClientSession:
-        if self._raw_session is None:
-            self._raw_session = aiohttp.ClientSession(
-                headers={"Content-Type": "application/json"}
-            )
-        return self._raw_session
-
-    @_session.setter
-    def _session(self, value):
-        self._raw_session = value
-
-    async def __aenter__(self) -> "EcstasyConnector":
-        return self
+    def session(self) -> aiohttp.ClientSession:
+        if self._session is None:
+            self._session = aiohttp.ClientSession(base_url=self._base_url, headers=self.headers)
+        return self._session
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         if self._session and not self._session.closed:
             await self._session.close()
             self._session = None
 
-
-class EcstasyService:
-    connector: EcstasyConnector = EcstasyConnector()
-
     async def get_interfaces_workload(
         self, devices_names: List[str]
     ) -> List[Dict[str, int]]:
-        async with self.connector as conn:
+        async with self.session as conn:
             tasks = [
                 self._get_interfaces_workload(connection=conn, device_name=devices_name)
                 for devices_name in devices_names
@@ -96,12 +35,10 @@ class EcstasyService:
 
     @staticmethod
     async def _get_interfaces_workload(connection, device_name: str) -> Dict[str, int]:
-        resp = await connection.request(
-            "get", f"/device/api/workload/interfaces/{device_name}"
-        )
+        resp = await connection.get(f"/api/v1/devices/workload/interfaces/{device_name}")
         if resp.status == 200:
             return await resp.json()
         return {}
 
 
-ecstasy_service = EcstasyService()
+ecstasy_service = EcstasyService(base_url=os.environ["ECSTASY_URL"], token=os.environ["ECSTASY_TOKEN"])
--- settings.py
+++ settings.py
@@ -1,10 +1,16 @@
 import os
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.memory import MemoryStorage
 
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="6326983114:AAFvRIdFH9zGBcn4oQVmKEqfJmRnzuqgk1g")
+if os.environ.get("PROXY"):
+    session = AiohttpSession(proxy=os.environ.get("PROXY"))
+else:
+    session = AiohttpSession()
+
+bot = Bot(token="6326983114:AAFvRIdFH9zGBcn4oQVmKEqfJmRnzuqgk1g", session=session)
 dp = Dispatcher(storage=MemoryStorage())
